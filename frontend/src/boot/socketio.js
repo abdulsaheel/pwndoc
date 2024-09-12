21cb88c7d4610c2025fab897bd203fc02a4bbe16
Mohammad Abdul Sahil || Python Programmer <127765312+abdulsaheel@users.noreply.github.com>
2024-09-12 11:12:25 +0530

    Update socketio.js

--- frontend/src/boot/socketio.js
+++ frontend/src/boot/socketio.js
@@ -1,9 +1,15 @@
 import io from 'socket.io-client'
-import { Loading } from 'quasar'
+import {Loading} from 'quasar'
 import { $t } from '@/boot/i18n'
 
 export default ({ Vue }) => {
-    var socketUrl = "http://127.0.0.1:4242";
+    var socketUrl = `${window.location.protocol}//${window.location.hostname}`;
+    if (process.env.API_PORT) {
+       socketUrl = socketUrl + ":" + process.env.API_PORT;
+    } else if (window.location.port) {
+       socketUrl = socketUrl + ":" + window.location.port;
+    }
+
     var socket = io(socketUrl);
 
     socket.on('disconnect', (error) => {
@@ -15,10 +21,9 @@ export default ({ Vue }) => {
             delay: 5000
         })
     })
-
     socket.on('connect', () => {    
         Loading.hide()
     })
 
     Vue.prototype.$socket = socket
-}
+}  
