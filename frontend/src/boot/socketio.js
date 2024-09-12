113206366159ce72b85aa8abad4bd6c76aea4dfd
Mohammad Abdul Sahil || Python Programmer <127765312+abdulsaheel@users.noreply.github.com>
2024-09-12 10:57:48 +0530

    Update socketio.js

--- frontend/src/boot/socketio.js
+++ frontend/src/boot/socketio.js
@@ -1,11 +1,9 @@
 import io from 'socket.io-client'
-import {Loading} from 'quasar'
+import { Loading } from 'quasar'
 import { $t } from '@/boot/i18n'
 
 export default ({ Vue }) => {
     var socketUrl = "https://127.0.0.1:4242";
-    }
-
     var socket = io(socketUrl);
 
     socket.on('disconnect', (error) => {
@@ -17,9 +15,10 @@ export default ({ Vue }) => {
             delay: 5000
         })
     })
+
     socket.on('connect', () => {    
         Loading.hide()
     })
 
     Vue.prototype.$socket = socket
-}  
+}
